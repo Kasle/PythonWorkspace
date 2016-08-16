b89e1938ca6e3648866b31ab5bba0ed02dc38629
Aleks Mercik <aleks.mercik@outlook.com>
2016-08-16 16:00:35 -0400

    Some comments

--- Ark Date Time/arkDateTime.py
+++ Ark Date Time/arkDateTime.py
@@ -1,44 +1,44 @@
-class time:
-    def __init__(self, time = "00:00:00"):
-        self.t = time
+class time: #Main Class
+    def __init__(self, time = "00:00:00"): #Create blank time, user input
+        self.t = time #Set variables
         self.s = float(time.split(":")[2])
         self.m = int(time.split(":")[1])
         self.h = int(time.split(":")[0])
         
-    def __add__(self, other):
-        self.s+=other.s
+    def __add__(self, other): #Adding time (Main use)
+        self.s+=other.s #Add variables from other time
         self.m+=other.m
         self.h+=other.h
         
         #print self.s, self.m, self.h
         
-        if self.s >= 60:
+        if self.s >= 60: #Crop seconds
             tmin = int(self.s / 60)
             self.s-=60 * (tmin)
             self.m+=tmin
             
         #print self.s, self.m, self.h
         
-        if self.m >= 60:
+        if self.m >= 60: #Crop minutes
             thour = (self.m / 60)
             self.m-=60 * (thour)
             self.h+=thour
         
         #print self.s, self.m, self.h        
         
-        if self.h >= 24:
+        if self.h >= 24: #Crop hours
             hmult = self.h / 24
             self.h-=24 * hmult
-        self.update()
-        return self
+        self.update() #Update string
+        return self #Return the new time
     
-    def __sub__(self, other):
+    def __sub__(self, other): #Subtract two times
         
         self.s -= other.s
         self.m -= other.m
         self.h -= other.h
         
-        mult = abs(int(self.s / 60))
+        mult = abs(int(self.s / 60)) #rectify and crop seconds, minutes, and hours
         self.s += 60 * mult
         if self.s < 0:
             self.s+=60
@@ -57,15 +57,15 @@ class time:
         if self.h < 0:
             self.h+=24
         
-        self.update() 
+        self.update() #Update string
         
-        return self
+        return self #Return
         
-    def __str__(self):
+    def __str__(self): #Print function
         self.update()
         return self.t
         
-    def update(self):
+    def update(self): #Update string from stored values
         ts = str(self.s)
         if len(ts.split(".")[0]) < 2:
             ts = "0"+ts
@@ -76,6 +76,10 @@ class time:
         if len(th) < 2:
             th = "0"+th
         self.t = str(th)+":"+str(tm)+":"+str(ts)
+        
+###
+###Example Of Use
+###        
         
 #a = time("22:00:55")
 #b = time("00:00:1")
--- updateLibraries.bat
+++ updateLibraries.bat
@@ -1,2 +1,3 @@
 xcopy "./Custom Functions" "C:\Users\Arkyd-Laptop\AppData\Local\Continuum\Anaconda2\Lib" /y /c
+xcopy "./Ark Date Time" "C:\Users\Arkyd-Laptop\AppData\Local\Continuum\Anaconda2\Lib" /y /c
 pause
