dffddadb99fbab931710ccb54842decf8ebf1780
Aleks Mercik <aleks.mercik@outlook.com>
2017-03-08 23:00:50 -0500

    added comments to main file. comments for neural network library coming up.

--- Network Approximation/Approx.py
+++ Network Approximation/Approx.py
@@ -1,21 +1,25 @@
-from NNLib import Network as Net;
-import math;
-import random;
+## Commented by: Aleksander Mercik Mar 2017
+## ========================================
+## Code uses a neural network to approximate NOT ( XOR ).
 
-for j in range(50):
-    avg = []
-    for i in range(10):
-        N = Net(sizes=[2, 1, 1]);
-        count = 0
-        while True:
-            count += 1
-            inp = [random.randint(0, 1), random.randint(0, 1)]
-            out = 0
+from NNLib import Network as Net #Import libraries
+import math
+import random
+
+for j in range(50): #Test the following 50 times
+    avg = [] #unused list
+    for i in range(10): #Unused loop / useless
+        N = Net(sizes=[2, 1, 1]) #Create a netowrk object
+        count = 0 #unused count
+        while True: #forever
+            count += 1 #incrament the count
+            inp = [random.randint(0, 1), random.randint(0, 1)] #set the input
+            out = 0 #the following lines set the output
             if sum(inp) == 0 or sum(inp) == 2:
                 out = 1
-            N.backprop(inp, [out])
-            inp = [random.randint(0, 1), random.randint(0, 1)]
-            print N.forward(inp)[0]
+            N.backprop(inp, [out]) #call the network learn function. give it the input and the output
+            inp = [random.randint(0, 1), random.randint(0, 1)] #get another input
+            print N.forward(inp)[0] #display to the user the output.
 #count = 0;
 #while True:
 #    count+=1;
